0e27b35f8f940c5e0bc8e92254e3623c10dd88a6
qwe5507 <apvmffkdls@gmail.com>
2023-07-11 22:59:24 +0900

    웹팩과 바벨

---
+++ lecture/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+    name: 'wordrelay-setting',
+    mode: 'development', // 실서비스 : production
+    devtool: 'eval',
+    resolve: {
+        extensions: ['.js', '.jsx'] // 밑의 엔트리 파일명의, 확장자 알아서 찾아줌
+    },
+
+    entry: {
+        // client.jsx안에 WordRelay.jsx를 불러오기때문에 안적어줘도 된다.
+        app: ['./client.jsx']
+    }, // 입력
+
+    module: {
+        rules: [{
+            test: /\.jsx?/,
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env', '@babel/preset-react'],
+                plugins: ['@babel/plugin-proposal-class-properties']
+            }
+        }]
+    },
+
+    output: {
+        path: path.join(__dirname, 'dist'), // 현재폴더경로/dist
+        filename: 'app.js'
+    }, // 출력
+}
