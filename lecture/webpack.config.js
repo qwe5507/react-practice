c99431dac7b3266f1fd08a34fc5642703a3d8ce6
qwe5507 <apvmffkdls@gmail.com>
2023-07-13 22:46:38 +0900

    핫 리로딩

--- lecture/webpack.config.js
+++ lecture/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
     name: 'wordrelay-setting',
@@ -19,13 +20,24 @@ module.exports = {
             loader: 'babel-loader',
             options: {
                 presets: ['@babel/preset-env', '@babel/preset-react'],
-                plugins: ['@babel/plugin-proposal-class-properties']
+                plugins: [
+                    '@babel/plugin-proposal-class-properties',
+                    'react-refresh/babel'
+                ]
             }
         }]
     },
-
+    plugins: [
+        new RefreshWebpackPlugin()
+    ],
     output: {
         path: path.join(__dirname, 'dist'), // 현재폴더경로/dist
-        filename: 'app.js'
+        filename: 'app.js',
+        publicPath: '/dist',
     }, // 출력
+    devServer: {
+        devMiddleware: { publicPath: '/dist' },
+        static: { directory: path.resolve(__dirname) },
+        hot: true,
+    }
 }
