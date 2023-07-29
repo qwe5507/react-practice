9561c8822fb5bd093f5ead6ade6998228f2b92da
qwe5507 <apvmffkdls@gmail.com>
2023-07-29 15:08:55 +0900

    테이블 최적화

--- lecture/Table.jsx
+++ lecture/Table.jsx
@@ -4,7 +4,9 @@ import Tr from "./Tr";
 const Table = ({ onClick, tableData, dispatch }) => {
     return (
         <table>
-            {Array(tableData.length).fill().map((tr, i) => (<Tr rowIndex={i} rowData={tableData[i]} dispatch={dispatch}></Tr>))}
+            {Array(tableData.length).fill().map((tr, i) => (
+                <Tr key={i} rowIndex={i} rowData={tableData[i]} dispatch={dispatch}></Tr>
+            ))}
         </table>
     )
 };
--- lecture/Td.jsx
+++ lecture/Td.jsx
@@ -1,7 +1,7 @@
-import React, { useCallback, useEffect, useRef } from 'react';
+import React, { useCallback, useEffect, useRef, memo } from 'react';
 import {CHANGE_TURN, CLICK_CELL} from "./TicTacToe";
 
-const Td = ({ rowIndex, cellIndex, dispatch, cellData}) => {
+const Td = memo(({ rowIndex, cellIndex, dispatch, cellData}) => {
     console.log('td rendered');
 
     const ref = useRef([]);
@@ -23,6 +23,6 @@ const Td = ({ rowIndex, cellIndex, dispatch, cellData}) => {
     return (
         <td onClick={onClickTd}>{cellData}</td>
     )
-};
+});
 
 export default Td;
