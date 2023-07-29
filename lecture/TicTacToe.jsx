b92f29647e90e116717e9b1242fe54fc52d5d35f
qwe5507 <apvmffkdls@gmail.com>
2023-07-29 14:55:15 +0900

    틱택토 구현

--- lecture/TicTacToe.jsx
+++ lecture/TicTacToe.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useReducer, useCallback } from 'react';
+import React, { useState, useEffect, useReducer, useCallback } from 'react';
 import Table from "./Table";
 
 const initialState = {
@@ -9,11 +9,13 @@ const initialState = {
         ['', '', ''],
         ['', '', '']
     ],
+    recentCell: [-1, -1],
 };
 
 export const SET_WINNER = 'SET_WINNER';
 export const CLICK_CELL = 'CLICK_CELL';
 export const CHANGE_TURN = 'CHANGE_TURN';
+export const RESET_GAME = 'RESET_GAME';
 
 const reducer = (state, action) => { // state를 어떻게 변경할지 작성
     switch (action.type) {
@@ -30,6 +32,7 @@ const reducer = (state, action) => { // state를 어떻게 변경할지 작성
             return {
                 ...state,
                 tableData: tableData,
+                recentCell: [action.row, action.cell],
             };
         case CHANGE_TURN: {
             return {
@@ -37,11 +40,26 @@ const reducer = (state, action) => { // state를 어떻게 변경할지 작성
                 turn: state.turn === 'O' ? 'X' : 'O',
             }
         }
+        case RESET_GAME: {
+            return {
+                ...state,
+                turn: 'O',
+                tableData: [
+                    ['', '', ''],
+                    ['', '', ''],
+                    ['', '', '']
+                ],
+                recentCell: [-1, -1],
+            };
+        }
+        default:
+            return state;
     }
 }
 
 const TicTacToe = () => {
     const [state, dispatch] = useReducer(reducer, initialState);
+    const { tableData, turn, winner, recentCell } = state;
 
     // const [winner, setWinner] = useState('');
     // const [turn, setTurn] = useState('o');
@@ -50,10 +68,50 @@ const TicTacToe = () => {
         dispatch({ type: SET_WINNER, winner: 'O' });
     }, []);
 
+    useEffect(() => {
+        const [row, cell] = recentCell;
+        if (row < 0) {
+            return;
+        }
+        let win = false;
+        if (tableData[row][0] === turn && tableData[row][1] === turn && tableData[row][2] === turn) { // 가로줄 검사
+            win = true;
+        }
+        if (tableData[0][cell] === turn && tableData[1][cell] === turn && tableData[2][cell] === turn) { //새로 줄 검사
+            win = true;
+        }
+        if (tableData[0][0] === turn && tableData[1][1] === turn && tableData[2][2] === turn) { // 대각선 검사
+            win = true;
+        }
+        if (tableData[0][2] === turn && tableData[1][1] === turn && tableData[2][0] === turn) { // 대각선 검사
+            win = true;
+        }
+        if (win) { // 승리시
+            dispatch({ type: SET_WINNER, winner: turn}) // 현재 턴인 사람이 승리자
+            dispatch({ type: RESET_GAME });
+        } else {
+            // 무승부 검사
+            let all = true;
+            tableData.forEach((row) => { // 칸이 다 차있는지 검사
+                row.forEach((cell) => {
+                    if (!cell) {
+                        all = false;
+                    }
+                });
+            });
+            if (all) {
+                dispatch({ type: SET_WINNER, winner: null});
+                dispatch({ type: RESET_GAME });
+            } else {
+                dispatch({ type: CHANGE_TURN });
+            }
+        }
+    }, [recentCell]);
+
     return (
         <>
-            <Table onClick={onClickTable} tableData={state.tableData} dispatch={dispatch}/>
-            {state.winner && <div>{state.winner}님의 승리</div>}
+            <Table onClick={onClickTable} tableData={tableData} dispatch={dispatch}/>
+            {winner && <div>{winner}님의 승리</div>}
         </>
     )
 };
