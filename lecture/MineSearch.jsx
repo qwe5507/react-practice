73eb4d1bb408e879f6d210eace69c1ddf2840d43
qwe5507 <apvmffkdls@gmail.com>
2023-07-30 18:28:08 +0900

    지뢰찾기 구현

--- lecture/MineSearch.jsx
+++ lecture/MineSearch.jsx
@@ -1,17 +1,91 @@
-import React, { useReducer, createContext } from 'react';
+import React, { useReducer, createContext, useMemo } from 'react';
 import Table from "./Table";
 import Form from "./Form";
 
-const TableContext = createContext();
+export const CODE = {
+    MINE: -7,
+    NORMAL: -1,
+    QUESTION: -2,
+    FLAG: -3,
+    QUESTION_MINE: -4,
+    FLAG_MINE: -5,
+    CLICKED_MINE: -6,
+    OPENED: 0, // 0 이상이면 다 opened
+};
+
+export const TableContext = createContext({
+    tableData: [],
+    halted: true,
+    dispatch: () => {},
+});
 
 const initialState = {
     tableData: [],
+    data: {
+        row: 0,
+        cell: 0,
+        mine: 0,
+    },
     timer: 0,
     result: '',
+    halted: true,
+    openedCount: 0,
 };
+
+
+const plantMine = (row, cell, mine) => {
+    console.log(row, cell, mine);
+    const candidate = Array(row * cell).fill().map((arr, i) => {
+        return i;
+    });
+    const shuffle = [];
+    while (candidate.length > row * cell - mine) {
+        const chosen = candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0];
+        shuffle.push(chosen);
+    }
+    const data = [];
+    for (let i = 0; i < row; i++) {
+        const rowData = [];
+        data.push(rowData);
+        for (let j = 0; j < cell; j++) {
+            rowData.push(CODE.NORMAL);
+        }
+    }
+
+    for (let k = 0; k < shuffle.length; k++) {
+        const ver = Math.floor(shuffle[k] / cell);
+        const hor = shuffle[k] % cell;
+        data[ver][hor] = CODE.MINE;
+    }
+
+    console.log(data);
+    return data;
+};
+
+export const START_GAME = 'START_GAME';
+export const OPEN_CELL = 'OPEN_CELL';
+export const CLICK_MINE = 'CLICK_MINE';
+export const FLAG_CELL = 'FLAG_CELL';
+export const QUESTION_CELL = 'QUESTION_CELL';
+export const NORMALIZE_CELL = 'NORMALIZE_CELL';
+export const INCREMENT_TIMER = 'INCREMENT_TIMER';
 
 const reducer = (state, action) => {
     switch (action.type) {
+        case START_GAME:
+            return {
+                ...state,
+                data: {
+                    row: action.row,
+                    cell: action.cell,
+                    mine: action.mine,
+                },
+                openedCount: 0,
+                tableData: plantMine(action.row, action.cell, action.mine),
+                result: '',
+                halted: false,
+                timer: 0,
+            };
         default:
             return state;
     }
@@ -19,14 +93,17 @@ const reducer = (state, action) => {
 
 const MineSearch = () => {
     const [state, dispatch] = useReducer(reducer, initialState);
+    const { tableData, halted, timer, result } = state;
+
+    const value = useMemo(() => ({ tableData, dispatch }), [tableData]);
 
     return (
-        <>
+        <TableContext.Provider value={value}>
             <Form></Form>
-            <div>{state.timer}</div>
+            <div>{timer}</div>
             <Table></Table>
-            <div>{state.result}</div>
-        </>
+            <div>{result}</div>
+        </TableContext.Provider>
     )
 };
 
--- lecture/Table.jsx
+++ lecture/Table.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import Tr from "./Tr";
+import {TableContext} from "./MineSearch";
 
 const Table = () => {
+    const {tableData} = useContext(TableContext);
+    return (
+        <table>
+            {Array(tableData.length).fill().map((tr, i) => <Tr rowIndex={i}></Tr>)}
+        </table>
 
+    )
 };
 
 export default Table;
